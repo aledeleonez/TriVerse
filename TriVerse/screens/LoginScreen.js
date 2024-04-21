e54cc9c745edcf866007578ee5fc02bbbc12ffd5
Alejandro de León Fernández <aledeleonez@gmail.com>
2024-04-21 16:36:36 +0200

    [ADD] LogIn and SignUp added

--- TriVerse/screens/LoginScreen.js
+++ TriVerse/screens/LoginScreen.js
@@ -1,35 +1,61 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
   return (
-    <View className="bg-white h-full w-full">
-      <View className="h-full w-full flex justify-around pt-40 pb-10">
-        <View className="flex items-center">
-            <Text className="text-white font-bold tracking-wider text-5xl">
-                Login
-            </Text>
+    <View className='bg-[#007EA7] h-full w-full'>
+      <StatusBar style='light' />
+      <View className='h-full w-full flex justify-around pt-40 pb-10'>
+        <View className='flex items-center'>
+          <Text className='text-white font-bold tracking-wider text-5xl'>
+            Login
+          </Text>
         </View>
-        <View className="flex items-center mx-4 space-y-4">
-            <View className="bg-black/5 p-5 rounded-2xl w-full">
-                <TextInput placeholder='Email' placeholderTextColor={'gray'}/>
-            </View>
-            <View className="bg-black/5 p-5 rounded-2xl w-full mb-3">
-                <TextInput placeholder='Contraseña' placeholderTextColor={'gray'} secureTextEntry/>
-            </View>
-            <View className="w-full">
-                <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
-                    <Text className="text-xl font-bold text-white text-center">Login</Text>
-                </TouchableOpacity>
-            </View>
-            <View className="flex-row justify-center">
-                <Text>No tienes una cuenta?</Text>
-                <TouchableOpacity>
-                    <Text className="text-sky-600">Registrate</Text>
-                </TouchableOpacity>
-            </View>
+        <View className='flex items-center mx-4 space-y-4'>
+          <View className='bg-[#D9D9D9] p-5 rounded-2xl w-full'>
+            <TextInput placeholder='Email' placeholderTextColor={"gray"} />
+          </View>
+          <View className='bg-[#D9D9D9] p-5 rounded-2xl w-full mb-3'>
+            <TextInput
+              placeholder='Contraseña'
+              placeholderTextColor={"gray"}
+              secureTextEntry
+            />
+          </View>
+          <View className='w-full'>
+            <TouchableOpacity className='w-full bg-[#003249] p-3 rounded-2xl mb-3'>
+              <Text className='text-xl font-bold text-white text-center'>
+                Log in
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className='flex-row justify-center'>
+            <TouchableOpacity>
+              <Text className='text-white underline'>
+                Olvidaste la contraseña?
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className='w-full'>
+            <TouchableOpacity
+              className='w-full bg-[#003249] p-3 rounded-2xl mb-3'
+              onPress={() => navigation.push("SignUp")}
+            >
+              <Text className='text-xl font-bold text-white text-center'>
+                Registrarse
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
-  )
+  );
 }
--- TriVerse/screens/SignUpScreen.js
+++ TriVerse/screens/SignUpScreen.js
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function SignUpScreen() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>SignUpScreen</Text>
+    <View className='bg-[#007EA7] h-full w-full'>
+      <StatusBar style='light' />
+      <View className='h-full w-full flex justify-around pt-40 pb-10'>
+        <View className='flex items-center'>
+          <Text className='text-white font-bold tracking-wider text-5xl'>
+            Registro
+          </Text>
+        </View>
+        <View className='flex items-center mx-4 space-y-4'>
+          <View className='bg-[#D9D9D9] p-5 rounded-2xl w-full'>
+            <TextInput placeholder='Usuario' placeholderTextColor={"gray"} />
+          </View>
+          <View className='bg-[#D9D9D9] p-5 rounded-2xl w-full mb-3'>
+            <TextInput
+              placeholder='Email'
+              placeholderTextColor={"gray"}
+              secureTextEntry
+            />
+          </View>
+          <View className='bg-[#D9D9D9] p-5 rounded-2xl w-full mb-3'>
+            <TextInput
+              placeholder='Contraseña'
+              placeholderTextColor={"gray"}
+              secureTextEntry
+            />
+          </View>
+          <View className='w-full'>
+            <TouchableOpacity className='w-full bg-[#003249] p-3 rounded-2xl mb-3'>
+              <Text className='text-xl font-bold text-white text-center'>
+                Registrarse
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className='flex-row justify-center'>
+            <TouchableOpacity onPress={() => navigation.push("Login")}>
+              <Text className='text-white underline'>
+                Ya tienes una cuenta? Log in
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
